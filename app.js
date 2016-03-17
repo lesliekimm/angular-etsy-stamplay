861eac4f03713d7555c8b75e15d88ab787cdc456
leslie kimm <leslie.kimm@gmail.com>
2016-03-17 10:18:58 -0700

    create angular app

---
+++ app.js
@@ -0,0 +1,11 @@
+// app.js
+angular
+  .module('etsyApp', ['ngStamplay', 'ui.router', 'app.routes'])
+  .controller('MainController', MainController);
+
+/**
+ * The main controler for our app
+ */
+function MainController() {
+  var main = this;
+}
